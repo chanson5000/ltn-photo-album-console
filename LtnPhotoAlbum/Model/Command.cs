b34cb62d752588ca9b5debc789253a0dfb675fde
Cory <chanson5000@gmail.com>
2018-10-05 16:47:55 -0500

    Re-work and re-factor.

--- LtnPhotoAlbum/Model/Command.cs
+++ LtnPhotoAlbum/Model/Command.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace LtnPhotoAlbum.Model
 {
@@ -16,28 +15,35 @@ namespace LtnPhotoAlbum.Model
             PhotoAlbum
         };
 
-        private readonly string[] _commandArgs;
+        public string Arg { get; private set; }
+        public ushort? Param { get; private set; }
 
-        public Command(string commandString) => _commandArgs = Normalize(ToArgArray(commandString));
-
-        public Command(IReadOnlyList<string> commandArray) => _commandArgs = Normalize(commandArray);
-
-        public string GetArg() => _commandArgs[0];
-
-        public ushort? GetParam() => ushort.TryParse(_commandArgs[1], out var result) ? (ushort?)result : null;
-
-        private static string[] Normalize(IReadOnlyList<string> toNormalize)
+        public bool SetArg(string arg)
         {
-            var argument = ParseArg(toNormalize[0]);
-            var parameter = toNormalize.Count > 1 ? ParseParam(toNormalize[1]) : null;
-
-            return new[] { argument, parameter };
+            if (ParseArg(arg) != null)
+            {
+                Arg = arg;
+                return true;
+            }
+            Arg = null;
+            return false;
         }
 
-        private static string[] ToArgArray(string input) => string.IsNullOrWhiteSpace(input) ? new[] { null, (string)null } : input.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+        public bool SetParam(ushort? param)
+        {
+            if (param != null)
+            {
+                Param = param;
+                return true;
+            }
+            Param = null;
+            return false;
+        }
+
+        public bool SetParam(string param) => SetParam(ParseParam(param));
 
         private static string ParseArg(string input) => ValidArgs.Contains(input) ? input : null;
 
-        private static string ParseParam(string input) => ushort.TryParse(input, out var result) ? result.ToString() : null;
+        private static ushort? ParseParam(string input) => ushort.TryParse(input, out var result) ? (ushort?)result : null;
     }
 }
